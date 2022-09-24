5197ac24292830a47f3ea6445638a883f4041024
Roman Karki <82092038+rxmxndai@users.noreply.github.com>
2022-09-24 08:30:33 +0545

    Added Scripts of A * Algorithm

---
+++ scripts/NodeRecord.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NodeRecord
+{
+public GameObject Node;
+public Connection Connection;
+public float CostSoFar;
+public float EstimatedTotalCost;
+public NodeRecord()
+{
+}
+}
